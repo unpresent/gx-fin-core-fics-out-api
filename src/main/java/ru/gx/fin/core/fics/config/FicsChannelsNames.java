c9698e17769b81e3490a970d647effa0b6d557eb
Vladimir Gagarkin <vladimir.gagarkin@gmail.com>
2021-12-08 23:35:46 +0300

    v1.2.2. ChannelsNames -> FicsChannelsNames.

--- src/main/java/ru/gx/fin/core/fics/config/FicsChannelsNames.java
+++ src/main/java/ru/gx/fin/core/fics/config/FicsChannelsNames.java
@@ -1,8 +1,8 @@
 package ru.gx.fin.core.fics.config;
 
 @SuppressWarnings("unused")
-public abstract class ChannelsNames {
-    public abstract static class FicsSnapshots {
+public abstract class FicsChannelsNames {
+    public abstract static class Snapshots {
         public static final String CURRENCIES = "fics-snapshots.currencies";
         public static final String SECURITIES = "fics-snapshots.securities";
         public static final String DERIVATIVES = "fics-snapshots.derivatives";
